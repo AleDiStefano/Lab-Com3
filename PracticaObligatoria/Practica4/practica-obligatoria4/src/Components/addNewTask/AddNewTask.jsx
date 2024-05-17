b6f387c1b24a2ea4ef457c5d2e3cc3eb729aa9f7
AleDiStefano <distefanoalejandrom@gmail.com>
2024-05-16 22:23:10 -0300

    actualizacion practica4

--- PracticaObligatoria/Practica4/practica-obligatoria4/src/Components/addNewTask/AddNewTask.jsx
+++ PracticaObligatoria/Practica4/practica-obligatoria4/src/Components/addNewTask/AddNewTask.jsx
@@ -4,13 +4,10 @@ import propTypes from 'prop-types';
 
 const AddNewTask = ({ onHandleNewTask }) => {
     const [taskName, setTaskName] = useState('');
-    const [show, setShow] = useState(false);
-
 
     const onHandleTaskName = (event) => {
         setTaskName(event.target.value);
     };
-
 
     const onSubmitNewTask = (event) => {
         event.preventDefault();
@@ -21,7 +18,6 @@ const AddNewTask = ({ onHandleNewTask }) => {
                 name: taskName,
                 done: false
             }
-            setShow(false)
             onHandleNewTask(newTask)
             setTaskName('')
         } else {
@@ -30,17 +26,14 @@ const AddNewTask = ({ onHandleNewTask }) => {
     }
     return (
         <>
-            {show ? <Button variant="primary" type="button" onClick={onHandleView}>New Task</Button>
-                :
-                <Form onSubmit={onSubmitNewTask}>
-                    <Form.Group className="mb-3" controlId="fromName">
-                        <Form.Label>Task Name</Form.Label>
-                        <Form.Control type="name" value={taskName} onChange={onHandleTaskName} />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Agregar
-                    </Button>
-                </Form>}
+            <Form onSubmit={onSubmitNewTask}>
+                <Form.Group className="mb-3" controlId="fromName">
+                    <Form.Control type="name" placeholder='Write here the task name' value={taskName} onChange={onHandleTaskName} />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Add
+                </Button>
+            </Form>
         </>
     )
 }
